7bcb4613343854f404c65f7e42dd99719d428583
Gilbert <louisfelix.berthiaume@gmail.com>
2017-11-04 17:49:06 -0400

    Update generator.js

--- generator.js
+++ generator.js
@@ -34,7 +34,7 @@ function generate() {
 function get_short_url(long_url, login, api_key, func)
 {
     $.getJSON(
-        "http://api.bitly.com/v3/shorten?callback=?", 
+        "https://api.bitly.com/v3/shorten?callback=?", 
         { 
             "format": "json",
             "apiKey": api_key,
